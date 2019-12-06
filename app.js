0944f01ce673b56f24b043939ea3a3739e4cc521
Wylky Ramirez <41131665+kingtoniormz@users.noreply.github.com>
2019-12-05 23:11:11 -0600

    s

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
-function registrar(){
-    console.log('Diste un click')
-    var email= document.getElementById('correo').value;
-    var contrasena= document.getElementById('pass').value;
-    
-
-    firebase.auth().createUserWithEmailAndPassword(email, contrasena).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-
-    console.log(errorCode);
-    console.log(errorMessage);
-    // ...
-  });
+function registrar() {
+    console.log('Diste un click');
+    var email = document.getElementById('correo').value;
+    var contrasena = document.getElementById('pass').value;
+    firebase.auth().createUserWithEmailAndPassword(email, contrasena).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        console.log(email)
+        console.log(contrasena)
+        // ...
+    });
 }
-
-
-
-
-
-
-
-
 

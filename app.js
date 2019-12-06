236980586b6a3552b925163139803ac30c8ba8bf
Wylky Ramirez <41131665+kingtoniormz@users.noreply.github.com>
2019-12-05 22:08:05 -0600

    fire

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 function registrar(){
     console.log('Diste un click')
-
-
-// conseguir el valor que esta dentro de  el caudro ingrese correo y email
-    var email = document.getElementById('correo').value;
-    var contrasena = document.getElementById('contrasena').value;
-
+    var email= document.getElementById('correo').value;
+    var contrasena= document.getElementById('pass').value;
     
 
-    firebase.auth().createUserWithEmailAndPassword(email,contrasena).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode)
-        console.log(errorMessage)
-  
-        console.log(email)
-        console.log(contrasena)
-  
-        // ...
-      });
+    firebase.auth().createUserWithEmailAndPassword(email, contrasena).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
 
-     
-
+    console.log(errorCode);
+    console.log(errorMessage);
+    // ...
+  });
 }
 
 
@@ -30,10 +20,6 @@ function registrar(){
 
 
 
-service firebase.storage {
-  match /b/{bucket}/o {
-    match /{allPaths=**} {
-      allow read, write: if request.auth!=null;
-    }
-  }
-}
+
+
+

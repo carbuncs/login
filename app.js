005f9bb69ee7dd9da9514cae2c3d028673b58557
Wylky Ramirez <41131665+kingtoniormz@users.noreply.github.com>
2019-12-05 18:40:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -6,17 +6,21 @@ function registrar(){
     var email = document.getElementById('correo').value;
     var contrasena = document.getElementById('contrasena').value;
 
-    console.log(email)
-    console.log(contrasena)
+    
 
     firebase.auth().createUserWithEmailAndPassword(email , contrasena).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
+        console.log(errorCode)
+        console.log(errorMessage)
+  
+        console.log(email)
+        console.log(contrasena)
+  
         // ...
-      });email-contrasena.html
+      });
 
-      console.log(errorCode)
-      console.log(errorMessage)
+     
 
 }
